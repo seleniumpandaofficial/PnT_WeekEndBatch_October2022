9bd8dee290096dc5b5af716e78b0e668e88cb796
seleniumpandaofficial <seleniumpanda@gmail.com>
2022-12-04 15:05:28 -0500

    Dec 4 notes

---
+++ src/main/java/collections_understanding/Sample.java
@@ -0,0 +1,31 @@
+package collections_understanding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Sample {
+
+	public static void main(String[] args) {
+	
+		String[] news = new String[105]; //4th Dec 2022
+		
+		int[] arr = {1,2,3,4,5};
+		
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("1");
+		al.add("Selenium");
+		al.add("c");
+		
+		System.out.println(al.size());
+		
+		HashMap<String,String> hm = new HashMap<String, String>();
+		hm.put("101", "ABC");
+		hm.put("102", "ABCD");
+		hm.put("103", "ABCDE");
+		
+		
+		
+
+	}
+
+}
